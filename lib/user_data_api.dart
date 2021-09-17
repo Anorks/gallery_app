b2e6305559fbaee8cd9752461e79ccd1887dd264
Anorks <anorkorlaryea@gmail.com>
2021-09-17 15:40:55 +0000

    final gallery app project

---
+++ lib/user_data_api.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_1.jfif",
+    "userPost": "images/Picture_1.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_2.jpg",
+    "userPost": "images/picture_2.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_3",
+    "userPost": "images/picture_3.jfif",
+    "userPostLikes": "23",
+  }
+];
